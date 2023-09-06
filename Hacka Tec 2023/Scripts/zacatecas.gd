27f410fe93c5a8c24307d900e22d3da833bce7fe
iTzChrisDev <chrissarmiento200203@gmail.com>
2023-09-06 17:22:51 -0600

    Iconos Juegos

---
+++ Hacka Tec 2023/Scripts/zacatecas.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+func _on_button_fresnillo_pressed():
+	pass
+
+
+func _on_button_jerez_pressed():
+	pass # Replace with function body.
+
+
+func _on_button_plateros_pressed():
+	pass # Replace with function body.
